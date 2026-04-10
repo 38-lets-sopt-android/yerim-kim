709f68aae93ad23b8f3cf7ecd037c8ba909b357f
doorimng <doorimng@gmail.com>
2026-04-10 15:27:55 +0900

    refactor: 컬러 이름 변경
    Signed-off-by: doorimng <doorimng@gmail.com>

--- app/src/main/java/com/example/letssopt/ui/theme/Theme.kt
+++ app/src/main/java/com/example/letssopt/ui/theme/Theme.kt
@@ -11,31 +11,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryRed,
+    surface = Surface,
+    background = Background,
+    onPrimary = TextPrimary,
+    onSecondary = TextSecondary,
+    onBackground = Background,
+    onSurface = Surface,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    surface = Color(0xFFFFFBFE),
-    background = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryRed,
+    surface = Surface,
+    background = Background,
+    onPrimary = TextPrimary,
+    onSecondary = TextSecondary,
+    onBackground = Background,
+    onSurface = Surface,
 )
 
 @Composable
 fun LETSSOPTTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
